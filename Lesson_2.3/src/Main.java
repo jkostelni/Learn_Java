443a1ec2a9d2260c5047965163878e4334f6f913
jkostelni <104040559+jkostelni@users.noreply.github.com>
2022-04-20 21:33:00 -0400

    Add files via upload

---
+++ Lesson_2.3/src/Main.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+       int[] numbers = {2, 3, 5, 1, 4};
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
+}
