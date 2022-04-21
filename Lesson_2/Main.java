d9b771a6e1ea0e1483205acd8942d323c45dc674
jkostelni <104040559+jkostelni@users.noreply.github.com>
2022-04-20 20:45:10 -0400

    Add files via upload

---
+++ Lesson_2/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        byte age = 30;
+        long viewscount = 3_123_456_789L;
+        float Price = 10.99F;
+        char letter = 'A';
+        boolean IsEligible = false;
+
+
+
+        System.out.println(age);
+    }
+}
