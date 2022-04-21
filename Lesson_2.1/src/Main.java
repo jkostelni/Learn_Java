cda867eca2cfc439d3d0e87e58f11326180b0520
jkostelni <104040559+jkostelni@users.noreply.github.com>
2022-04-20 20:51:25 -0400

    Add files via upload

---
+++ Lesson_2.1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+
+
+
+        System.out.println(age);
+    }
+}
